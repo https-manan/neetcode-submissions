8ec00d2db63ae7331a89c6a2660ce7390a74470d
https-manan <166443202+https-manan@users.noreply.github.com>
2026-06-01 08:04:34 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t){
+        unordered_map<char,int>m;
+        for(char i:s){
+            m[i]++;
+        }
+        for(char i:t){
+            m[i]--;
+        }
+        for(auto &i:m){
+            if(i.second!=0)return false;
+        }
+        return true;
+    }
+};
