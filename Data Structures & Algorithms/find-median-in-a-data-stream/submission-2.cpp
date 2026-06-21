fd69c8bfe00978300c0410696e30ee618b647516
https-manan <166443202+https-manan@users.noreply.github.com>
2026-06-21 07:35:55 +0530

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.cpp
@@ -0,0 +1,22 @@
+class MedianFinder {
+public:
+    priority_queue<int> maxQ;
+    priority_queue<int, vector<int>, greater<int>> minQ;
+    MedianFinder() {
+    }
+    void addNum(int num) {
+        maxQ.push(num);
+        minQ.push(maxQ.top());
+        maxQ.pop();
+        if (minQ.size() > maxQ.size()) {
+            maxQ.push(minQ.top());
+            minQ.pop();
+        }
+    }
+    double findMedian() {
+        if (maxQ.size() > minQ.size()) {
+            return maxQ.top();
+        }
+        return (maxQ.top() + minQ.top()) / 2.0;
+    }
+};
