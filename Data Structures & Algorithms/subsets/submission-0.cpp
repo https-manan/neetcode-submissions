db14807ffd3081fb730631e77fdcbc83dbd35036
https-manan <166443202+https-manan@users.noreply.github.com>
2026-06-23 09:16:05 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void helper(vector<int>& nums,vector<int>&temp,int i){
+        if(i==nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        temp.push_back(nums[i]);
+        helper(nums,temp,i+1);
+        temp.pop_back();
+        helper(nums,temp,i+1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums){
+        vector<int>temp;
+        ans.push_back(temp);
+        helper(nums,temp,0);
+        ans.pop_back();
+        return ans;
+    }
+};
