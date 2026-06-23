1e9ad1dd24cdd6a2839bdd172f8275ff915f7415
https-manan <166443202+https-manan@users.noreply.github.com>
2026-06-23 10:22:52 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void helper(vector<int>& nums, int t,
+                vector<int>& temp, int sum, int i) {
+
+        if (sum == t) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (i == nums.size() || sum > t)
+            return;
+
+        temp.push_back(nums[i]);
+        helper(nums, t, temp, sum + nums[i], i); // reuse allowed
+        temp.pop_back();
+        helper(nums, t, temp, sum, i + 1);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> temp;
+        helper(nums, target, temp, 0, 0);
+        return ans;
+    }
+};
